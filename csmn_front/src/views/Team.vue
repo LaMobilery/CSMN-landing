8061d87f9ae037ead09261dd112dcb070881327c
cedricrenouleau <ced241@gmail.com>
2024-08-21 15:20:11 +0200

    tri alpha team

--- csmn_front/src/views/Team.vue
+++ csmn_front/src/views/Team.vue
@@ -24,19 +24,17 @@ import wave1 from '@/assets/waves/wave1.svg'
 import wave2 from '@/assets/waves/wave2.svg'
 import wave3 from '@/assets/waves/wave13.svg'
 import TeamCard from '@/components/TeamCard.vue'
-import president from '@/assets/images/team/president.jpeg'
 import team1 from '@/assets/images/team/team1.png'
 import team2 from '@/assets/images/team/team2.jpeg'
 import team3 from '@/assets/images/team/team3.jpeg'
 import team4 from '@/assets/images/team/team4.jpeg'
-import team5 from '@/assets/images/team/team5.jpeg'
-import team6 from '@/assets/images/team/team6.png'
+import team5 from '@/assets/images/team/team5.png'
+import team6 from '@/assets/images/team/team6.jpeg'
 import team7 from '@/assets/images/team/team7.png'
 import team8 from '@/assets/images/team/team8.jpeg'
-import team9 from '@/assets/images/team/team9.jpeg'
+import team9 from '@/assets/images/team/team9.png'
 import team10 from '@/assets/images/team/team10.jpeg'
-import team11 from '@/assets/images/team/team11.png'
-
+import team11 from '@/assets/images/team/team11.jpeg'
 
 const TeamCards = [
   {
@@ -44,49 +42,59 @@ const TeamCards = [
     role: 'Président',
     name: 'Fabrice BELLOTTI',
     email: 'president@plongee-lille.fr'
-  },  {
+  },
+  {
     photo: team2,
     role: 'Secrétaire',
     name: 'Cécile CAULLERY',
     email: 'secretaire@plongee-lille.fr'
-  },  {
+  },
+  {
     photo: team3,
     role: 'Trésorier',
     name: 'David TAQUET',
     email: 'tresorier@plongee-lille.fr'
-  },  {
+  },
+  {
     photo: team4,
-    role: 'Président Adjoint',
-    name: 'Christophe QUIVRON',
-  },  {
-    photo: team5,
     role: 'Membre',
     name: 'Nathan BAELEN',
-  },  {
-    photo: team6,
-    role: 'Membre',
-    name: 'Bertrand DIGONNET',
-  },  {
-    photo: team7,
+  },
+  {
+    photo: team5,
     role: 'Membre',
     name: 'Sylvie CHERRIER',
-  },  {
-    photo: team8,
+  },
+  {
+    photo: team6,
     role: 'Membre',
     name: 'Harold DESCAMPS',
-  },  {
-    photo: team9,
+  },
+  {
+    photo: team7,
+    role: 'Membre',
+    name: 'Bertrand DIGONNET',
+  },
+  {
+    photo: team8,
     role: 'Membre',
     name: 'Pascal D’HARLINGUE',
-  },  {
-    photo: team10,
-    role: 'Membre',
-    name: 'Anna TOMCZAK',
-  },  {
-    photo: team11,
+  },
+  {
+    photo: team9,
     role: 'Membre',
     name: 'Jean-Michel MYENS',
-  }
+  },
+  {
+    photo: team10,
+    role: 'Président Adjoint',
+    name: 'Christophe QUIVRON',
+  },
+  {
+    photo: team11,
+    role: 'Membre',
+    name: 'Anna TOMCZAK',
+  },
 ]
 </script>
 
